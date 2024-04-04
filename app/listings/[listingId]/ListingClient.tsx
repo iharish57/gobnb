69888dd97388fcaf759995f738fe920e38cb41d4
iharish57 <iharish@live.com>
2024-04-04 20:07:33 +0530

    edit mxauto

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -105,7 +105,7 @@ const ListingClient:React.FC<ListingClientProps> = ({
 
   return (
     <Container>
-        <div className="max-w-screen-lg mxauto">
+        <div className="max-w-screen-lg mx-auto">
             <div className="flex flex-col gap-6">
                 <ListingHead
                     title={listing.title}
